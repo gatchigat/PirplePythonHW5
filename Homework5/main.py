e68a7e2f85b6ca15f71fd0f1a30a2a99e6ac1dc2
gatchigat <80865346+gatchigat@users.noreply.github.com>
2021-04-25 22:29:59 +0800

    Update main.py

--- Homework5/main.py
+++ Homework5/main.py
@@ -1,8 +1,8 @@
-def primeNo(x):
+def primeNo(x):  # Prime Numbers 1 to 100
     return x in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
 
-for counter in range(1, 101):
+for counter in range(1, 101): # Loop range 1 - 100
     num = ''
     if primeNo(counter):
         num = 'Prime'
